275f2606b032bc7f915beb3c8173629b4131aedb
Sai0698 <sairam.kotha@icloud.com>
2022-09-11 15:45:49 +0530

    second dice app

--- app/src/main/java/com/ksappdev/dicerollerapp/MainActivity.kt
+++ app/src/main/java/com/ksappdev/dicerollerapp/MainActivity.kt
@@ -3,7 +3,7 @@ package com.ksappdev.dicerollerapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,11 +17,35 @@ class MainActivity : AppCompatActivity() {
     private fun rolldice() {
 //        created instance and call class function
         val dice = Dice(6)
-        val diceRoll = dice.roll()
-        val textview: TextView = findViewById(R.id.TvDisplay)
-        textview.text = diceRoll.toString()
+        val diceRoll1 = dice.roll()
+        val diceRoll2 = dice.roll()
+        val imageview: ImageView = findViewById(R.id.IVdisplay)
+        val imageview2: ImageView = findViewById(R.id.IVdisplay2)
 
+        val drawableset1 = when (diceRoll1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val drawableset2 = when (diceRoll2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        imageview.setImageResource(drawableset1)
+        imageview2.setImageResource(drawableset2)
+
+        imageview.contentDescription = diceRoll1.toString()
+        imageview2.contentDescription = diceRoll2.toString()
     }
+
+
 }
 
 class Dice(private val numslides: Int) {
